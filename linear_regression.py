6b6aa39cac84686ab736e91d8cb6642f2209d79b
dgrajeda2005 <dgrajeda2005@yahoo.com.mx>
2024-10-23 21:00:04 -0400

    repo update and cleanup

---
+++ linear_regression.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+# Loading the Dataset
+
+df = sns.load_dataset("mpg")
